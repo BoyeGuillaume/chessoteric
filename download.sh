02a254587568592d7e4dbe86d29fbd6680947461
BoyeGuillaume <guillaume.boye@epfl.ch>
2026-02-17 19:46:08 +0100

    Rename and add download scripts

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/official-stockfish/books/raw/master/UHO_Lichess_4852_v1.epd.zip
+unzip UHO_Lichess_4852_v1.epd.zip
+rm UHO_Lichess_4852_v1.epd.zip
+mv UHO_Lichess_4852_v1.epd ./scripts/data/
